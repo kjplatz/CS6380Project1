a0dbb2f193a943fd42be796fe413a48a8fab973e
Joshua Olson <oaj2015>
2014-09-30 19:15:08 -0500

    First attempt at getting the processes to send and receive explore messages.

--- src/run_node.cpp
+++ src/run_node.cpp
@@ -17,36 +17,63 @@
 #include "CS6380Project1.h"
 
 using namespace std;
+
 //
 // Function to run a node
 // Parameters:
 //    node_id     : The node ID to represent
 //    master_fd   : Socket file descriptor to talk to/from master thread
 //    neighbor_fds: The socket file descriptors of my neighbors
-void run_node( int node_id, int master_fd,  vector<int> neighbor_fds ) {
-	int maxId = node_id;
-    ofstream fout( string{"node"} + to_string(node_id) + string{".log"} );
+void run_node(int node_id, int master_fd, vector<int> neighbor_fds) {
+    int maxId = node_id;
+    ofstream fout(string{"node"} + to_string(node_id) + string{".log"});
     fout << "Starting node " << node_id << endl;
 
-	int round = 0;
+    int round = 0;
 
-	while( round < 10 ) {
-		round++;
-	    Message msg(master_fd);
+    while (round < 10) {
+        round++;
+        Message msg(master_fd);
 
-		fout << "Node " << node_id << " received message from fd " << master_fd <<
-				": " << msg.toString() << endl;
+        fout << "Node " << node_id << " received message from fd " << master_fd <<
+                ": " << msg.toString() << endl;
 
-		if ( round == 9 ) {
-			Message done(Message::MSG_LEADER);
-		    fout << "Node " << node_id << " sending message " << done.toString() << endl;
-		    done.send( master_fd );
-		} else {
-			Message done(Message::MSG_DONE, round);
-			fout << "Node " << node_id << " sending message " << done.toString() << endl;
-			done.send( master_fd );
-		}
-	}
+        Message explr(Message::MSG_EXPLORE, maxId);
+
+        //Send maxId to all neighbors
+        for (auto fd : neighbor_fds) {
+            explr.send(fd);
+        }
+
+        //Receive message from all neighbors
+        for (auto fd : neighbor_fds) {
+            int receivedId;
+
+            Message msg(fd);
+
+            fout << "Node " << node_id << " received message from fd " << fd <<
+                    ": " << msg.toString() << endl;
+
+            switch (msg.msgType) {
+                case Message::MSG_EXPLORE:
+                    receivedId = msg.id;
+                    if (receivedId > maxId) {
+                        maxId = receivedId;
+                    }
+                default:
+                    throw runtime_error(
+                            string{"Process thread received unexpected message: "} + msg.toString());
+            }
+        }
+        
+        if (round == 9) {
+            Message done(Message::MSG_LEADER);
+            fout << "Node " << node_id << " sending message " << done.toString() << endl;
+            done.send(master_fd);
+        } else {
+            Message done(Message::MSG_DONE, round);
+            fout << "Node " << node_id << " sending message " << done.toString() << endl;
+            done.send(master_fd);
+        }
+    }
 }
-
-
